f61736e530f0c9d367d2bb8fc341ef8c2ae12b0e
hienphanbk <phanhien.fit@gmail.com>
2016-03-12 22:53:51 +0700

    initial

---
+++ test/com/viettel/demo/MainTest.java
@@ -0,0 +1,17 @@
+package com.viettel.demo;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hienpt on 3/12/16.
+ */
+public class MainTest extends TestCase {
+
+    @Test
+    public void testF2c() throws Exception {
+
+    }
+}
